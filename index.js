91c930230e50ce7240f35e9d36381cac148d2c23
Edwin <edwinzhou99@gmail.com>
2021-09-30 12:26:38 -0700

    turn bot on

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+require('dotenv').config();
 
+// const token ='ODkyODk2NzA3MzI5Nzk0MDU4.YVTk3A.4MUmpHoD-mBkkwIaeI1ZMJK9Wwk';
+
+bot.on('ready', ()=>{
+    console.log('RIP Stephen');
+});
+
+bot.on('message',()=>{
+    if(msg.content === 'hello'){
+        msg.replay('hello');
+    }
+});
+
+bot.login(process.env.token);
